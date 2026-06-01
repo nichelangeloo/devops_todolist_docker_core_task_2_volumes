060645279f8d1c0f9f287adb22abcb2fdf43e5ac
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-06-01 17:52:58 +0300

    fix

--- todolist/settings.py
+++ todolist/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'USER': 'app_user',
         'PASSWORD': '1234',
         # You can use a different host in your MySQL server is on a remote machine.
-        'HOST': '172.17.0.2',
+        'HOST': 'hw-mysql',
         'PORT': '',  # Leave this empty to use the default MySQL port (3306).
     }
 
